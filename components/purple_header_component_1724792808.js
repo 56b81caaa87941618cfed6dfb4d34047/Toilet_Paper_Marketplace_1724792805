6a968f2990a5fbc91325c214ed262e086a9a9685
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-27 17:23:06 -0400

    Merge code from frontend

--- components/purple_header_component_1724792808.js
+++ components/purple_header_component_1724792808.js
@@ -2,7 +2,7 @@
   <header class="w-full z-30" id="header-section-container" style="min-height: 390px">
     <div class="max-w-6xl mx-auto px-4 sm:px-6 bg-white">
       <div class="flex items-center justify-between h-16 md:h-20">
-        <!-- Site Branding -->
+        <!-- Site Branding   -->
         <div id="site-branding" class="flex-1">
           <a id="logo-link" class="inline-flex" href="index.html" aria-label="Cruip">
             <img id="logo-image" class="max-w-none" src="./images/logo.svg" width="38" height="38" alt="Stellar">
@@ -81,18 +81,6 @@
             </a>
           </li>
         </ul>
-
-        <!-- Hadoken Button -->
-        <div class="flex-1 flex justify-end items-center ml-4">
-          <button
-            id="hadoken-button"
-            class="btn-sm transition duration-150 ease-in-out text-white hover:text-yellow-300 bg-gradient-to-r from-blue-600 to-blue-400 hover:from-blue-500 hover:to-blue-300 rounded-full px-4 py-2 shadow-lg flex items-center"
-            @click="performHadoken"
-          >
-            <i class='bx bxs-hot mr-2'></i>
-            Hadoken!
-          </button>
-        </div>
 
         <!-- Mobile Menu -->
         <div class="md:hidden flex items-center ml-4">
@@ -190,13 +178,7 @@ export default {
       // Reset form after submission
       this.form = { name: '', email: '', message: '' };
     }
-    },
-    // Hadoken method
-    performHadoken() {
-      console.log("Hadoken!");
-      // Add your Hadoken logic here
-    }
-    // End of Hadoken method
+    // End of submit form method
   }
 };
 </script>
